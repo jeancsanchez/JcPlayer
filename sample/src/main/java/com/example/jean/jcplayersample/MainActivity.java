18dd485b6e4cbd48c00ae4f858c0f33752354366
Jean Carlos <jeancs.patricio@gmail.com>
2016-09-01 16:21:09 -0300

    new way of initialize audios

--- sample/src/main/java/com/example/jean/jcplayersample/MainActivity.java
+++ sample/src/main/java/com/example/jean/jcplayersample/MainActivity.java
@@ -10,41 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-
-    private List<JCAudio> JCAudioList;
     private JCPlayerView player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         player = (JCPlayerView) findViewById(R.id.jcplayer);
 
-        JCAudioList = new ArrayList<>();
-        JCAudio JCAudio1 = new JCAudio();
-        JCAudio1.setId(1);
-        JCAudio1.setPosition(1);
-        JCAudio1.setTitle("Ari - Alteza");
-        JCAudio1.setUrl("http://10.0.1.61:8080/Ari%20-%20Alteza%20(Part.%20Banda%20Cone).mp3");
-
-        JCAudio JCAudio2 = new JCAudio();
-        JCAudio2.setId(2);
-        JCAudio2.setPosition(2);
-        JCAudio2.setTitle("Cacife Clandestino - Eu e você contra o mundo");
-        JCAudio2.setUrl("http://10.0.1.61:8080/Cacife%20Clandestino%20-%20Eu%20e%20Voc%C3%AA%20Contra%20o%20Mundo%20part.%20Reis%20Do%20Nada.mp3");
-
-
-        JCAudioList.add(JCAudio1);
-        JCAudioList.add(JCAudio2);
-
-        player.initPlaylist(JCAudioList, this);
-
+        ArrayList<String> urls = new ArrayList<>();
+        urls.add("http://www.villopim.com.br/android/Music_01.mp3");
+        urls.add("http://www.villopim.com.br/android/Music_02.mp3");
+        player.initWithTitlePlaylist(urls, "Awesome music");
     }
 
     @Override
     public void onPause(){
         super.onPause();
-        player.createNotification(R.drawable.myicon);
+        player.createNotification();
     }
 }
