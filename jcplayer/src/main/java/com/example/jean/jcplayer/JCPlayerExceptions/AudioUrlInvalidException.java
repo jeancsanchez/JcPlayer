19778cafbe253d179a5053abc8874f6ba3273274
Jean Carlos <jeancs.patricio@gmail.com>
2016-09-01 15:57:02 -0300

    excpetion for catch invalids urls

--- jcplayer/src/main/java/com/example/jean/jcplayer/JCPlayerExceptions/AudioUrlInvalidException.java
+++ jcplayer/src/main/java/com/example/jean/jcplayer/JCPlayerExceptions/AudioUrlInvalidException.java
@@ -4,5 +4,8 @@ package com.example.jean.jcplayer.JCPlayerExceptions;
  * Created by jean on 01/09/16.
  */
 
-public class AudioUrlInvalidException {
+public class AudioUrlInvalidException extends Exception{
+    public AudioUrlInvalidException(String url){
+        super("The url does not appear valid: " + url);
+    }
 }
