dd30f9271853c26375e0b4aa2784b1b002e88d2b
jeancsanchez <jeancs.patricio@gmail.com>
2016-12-04 16:56:34 -0300

    updating examples

--- sample/src/main/java/com/example/jean/jcplayersample/MainActivity.java
+++ sample/src/main/java/com/example/jean/jcplayersample/MainActivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.jean.jcplayer.JcAudio;
+import com.example.jean.jcplayer.JcPlayerService;
 import com.example.jean.jcplayer.JcPlayerView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JcPlayerService.OnInvalidPathListener {
     private LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
     private JcPlayerView player;
     private RecyclerView recyclerView;
@@ -25,28 +27,44 @@ public class MainActivity extends AppCompatActivity {
         player = (JcPlayerView) findViewById(R.id.jcplayer);
 
         ArrayList<JcAudio> jcAudios = new ArrayList<>();
-        jcAudios.add(JcAudio.createFromURL("url audio","http://www.villopim.com.br/android/Music_01.mp3"));
-        jcAudios.add(JcAudio.createFromAssets("Asset audio", "49.v4.mid"));
-        jcAudios.add(JcAudio.createFromRaw("Raw audio", R.raw.a_34));
-        jcAudios.add(JcAudio.createFromFilePath("File directory audio", this.getFilesDir() + "/" + "13.mid"));
-        jcAudios.add(JcAudio.createFromAssets("I am invalid audio", "aaa.mid")); // invalid assets file
-        //urls.add(new FileAndOrigin("http://www.villopim.com.br/android/Music_02.mp3", Origin.URL));
-        //jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "13.mid"));
-        //jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "123123.mid")); // invalid file path
+//        jcAudios.add(JcAudio.createFromURL("url audio","http://www.villopim.com.br/android/Music_01.mp3"));
+//        jcAudios.add(JcAudio.createFromAssets("Asset audio", "49.v4.mid"));
+//        jcAudios.add(JcAudio.createFromRaw("Raw audio", R.raw.a_34));
+//        jcAudios.add(JcAudio.createFromFilePath("File directory audio", this.getFilesDir() + "/" + "CANTO DA GRAÚNA.mp3"));
+//        jcAudios.add(JcAudio.createFromAssets("I am invalid audio", "aaa.mid")); // invalid assets file
+//        player.initPlaylist(jcAudios);
+
+
+//        jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "13.mid"));
+//        jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "123123.mid")); // invalid file path
 //        jcAudios.add(JcAudio.createFromAssets("49.v4.mid"));
 //        jcAudios.add(JcAudio.createFromRaw(R.raw.a_203));
-        //jcAudios.add(JcAudio.createFromRaw("a_34", R.raw.a_34));
-        //urls.add(new FileAndOrigin(String.valueOf(1), Origin.RAW)); // invalid raw file.
+//        jcAudios.add(JcAudio.createFromRaw("a_34", R.raw.a_34));
 //        player.initWithTitlePlaylist(jcAudios, "Awesome music");
-        player.initPlaylist(jcAudios);
 
 
+//        jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "13.mid"));
+//        jcAudios.add(JcAudio.createFromFilePath("test", this.getFilesDir() + "/" + "123123.mid")); // invalid file path
+//        jcAudios.add(JcAudio.createFromAssets("49.v4.mid"));
+//        jcAudios.add(JcAudio.createFromRaw(R.raw.a_203));
+//        jcAudios.add(JcAudio.createFromRaw("a_34", R.raw.a_34));
+//        player.initAnonPlaylist(jcAudios);
+
+
+        // Adding new audios to playlist
+        player.addAudio(JcAudio.createFromURL("url audio","http://www.villopim.com.br/android/Music_01.mp3"));
+        player.addAudio(JcAudio.createFromAssets("49.v4.mid"));
+        player.addAudio(JcAudio.createFromRaw(R.raw.a_34));
+        player.addAudio(JcAudio.createFromFilePath(this.getFilesDir() + "/" + "121212.mmid"));
+
+        player.registerInvalidPathListener(this);
         adapterSetup();
     }
 
 
     public void playAudio(JcAudio jcAudio){
         player.playAudio(jcAudio);
+        Toast.makeText(this, player.getCurrentAudio().getOrigin().toString(), Toast.LENGTH_SHORT).show();
     }
 
     protected void adapterSetup() {
@@ -55,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(audioAdapter);
         audioAdapter.setupItems(player.getMyPlaylist());
     }
-
 
     @Override
     public void onPause(){
@@ -67,5 +84,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         player.kill();
+    }
+
+    @Override
+    public void onPathError(JcAudio jcAudio) {
+        Toast.makeText(this, jcAudio.getPath() + " with problems", Toast.LENGTH_LONG).show();
+//        player.removeAudio(jcAudio);
+//        player.next();
     }
 }
