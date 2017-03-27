03c4e66bc4019c38b2229ca569e1c4823bfd719f
jeancarlos <jeancs.patricio@gmail.com>
2017-03-27 16:29:09 -0300

    :construction: :umbrella: First test

--- jcplayer/src/test/java/com/example/jean/jcplayer/JcPlayerViewTest.java
+++ jcplayer/src/test/java/com/example/jean/jcplayer/JcPlayerViewTest.java
@@ -1,8 +1,48 @@
 package com.example.jean.jcplayer;
 
+import android.content.Context;
+import android.support.annotation.RawRes;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.MockUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by jeancarlos on 3/27/17.
  */
 
 public class JcPlayerViewTest {
+
+    @Mock
+    Context context;
+
+    private JcPlayerView jcPlayerView;
+    private List<JcAudio> playlist;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        jcPlayerView = new JcPlayerView(context);
+
+
+        playlist = new ArrayList<>();
+        playlist.add(JcAudio.createFromAssets("fake asset"));
+        playlist.add(JcAudio.createFromURL("fake url"));
+        playlist.add(JcAudio.createFromFilePath("fake file path"));
+
+        jcPlayerView.initPlaylist(playlist);
+
+    }
+
+    @Test
+    public void player_has_same_size_of_playlist_user(){
+        assertEquals(jcPlayerView.getMyPlaylist().size(), playlist.size());
+    }
 }
