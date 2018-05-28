7c5fbe29e47697b9271d80ef87c66b7de0c48119
jean <jeancs.patricio@gmail.com>
2018-05-28 14:18:58 -0300

    Extracting logic to Util class

---
+++ jcplayer/src/main/java/com/example/jean/jcplayer/general/JcPlayerUtil.kt
@@ -0,0 +1,10 @@
+package com.example.jean.jcplayer.general
+
+/**
+ * This class represents...
+ * @author Jean Carlos (Github: @jeancsanchez)
+ * @date 18/05/18.
+ * Jesus loves you.
+ */
+object PlayerUtil {
+}
